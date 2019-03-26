9f294fd3297caeb47e8fbd945fe0f091dcf2d6e0
Hongxu Jia <hongxu.jia@windriver.com>
2019-03-26 11:36:01 +0800

    face-detection: tweak position of shebang
    Signed-off-by: Hongxu Jia <hongxu.jia@windriver.com>

--- meta-demo/recipes-demo/face-detection/files/collect_face.py
+++ meta-demo/recipes-demo/face-detection/files/collect_face.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # Copyright (c) 2019, Wind River Systems, Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -17,7 +18,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-#!/usr/bin/python3
 import os
 import time
 import argparse
